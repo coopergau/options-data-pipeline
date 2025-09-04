c610d3f4fc4881f7a7f60820d24a24bfb0be01d2
root <cooperg122@gmail.com>
2025-09-04 17:05:18 -0600

    added dockerfile

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 import pandas as pd
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
+
+load_dotenv()
 
 def get_expiration_dates(expiration_dates, days_out=31):
     # Returns expiration dates that fall within the next 'days_out' days
@@ -30,12 +33,36 @@ def get_chain_df(ticker, exp_date, option_type, stock_price):
     chain['stock_price'] = stock_price
     return chain
 
+def connect_to_database():
+    try:
+        database_url = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
+        engine = create_engine(database_url)
+
+        # Test connection
+        with engine.connect() as connect:
+            pass
+        return engine
+    except Exception as e:
+        print(f'Error connecting to database: {e}')
+        raise
+   
+
+def save_to_database(df, table_name, engine):
+    try:
+        df.to_sql(table_name, engine, if_exists='append', index=False)
+        print(f'Successfuly saved {len(df)} rows to {table_name} table')
+    except Exception as e:
+        print(f'Error saving to database: {e}')
+        raise
 
 def main():
-    option_types = ['call', 'put']
+
+    engine = connect_to_database()
+
+    option_tables = {'call': 'call_options', 'put': 'put_options'}
     all_ticker_names = open('nasdaq100.txt').read().splitlines()
 
-    for option_type in option_types:
+    for option_type, table_name in option_tables.items():
         df_list = []
         
         for ticker_name in all_ticker_names:
@@ -49,6 +76,7 @@ def main():
                 df_list.append(chain)
             
         combined_df = pd.concat(df_list, ignore_index=True)
+        save_to_database(combined_df, table_name, engine)
 
 if __name__ == '__main__':
     main()
